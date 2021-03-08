2a85faf791435d30ad1e354ca1b2537a45c22b68
[Baligh <[lassouedba@gmail.com]>
2021-03-08 22:01:35 +0100

    first commit

---
+++ src/Components/Task.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { deleteTask, doneTask } from "../Redux/Actions/listTasks";
+import Edit from "./Edit/Edit";
+import { Button } from "react-bootstrap";
+
+const Task = ({ task }) => {
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <div className="task-card">
+        <span className={task.isDone ? "done" : null}>{task.text}</span>
+        <Edit task={task} />
+        <Button
+          variant="outline-primary"
+          onClick={() => dispatch(deleteTask(task.id))}
+        >
+          Delete
+        </Button>
+        <Button
+          variant="outline-primary"
+          onClick={() => dispatch(doneTask(task.id))}
+        >
+          {task.isDone ? "undone" : "done"}
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Task;
